2a869c47bae8a02e038cfc4b59ae4c459ac55827
ThanhLai <lainguyen523@gmail.com>
2020-04-13 11:36:13 +0700

    Model

---
+++ app/Models/Choices.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Choices extends Model
+{
+    protected $fillable = [
+    	'word_id',
+    	'content',
+        'correct',
+    ];
+
+    public function answers()
+    {
+    	return $this->hasOne(Answers::class);
+    }
+
+    public function words()
+    {
+    	return $this->belongsTo(Words::class);
+    }
+}
