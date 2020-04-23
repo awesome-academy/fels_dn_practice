6846a59d76ddc0dbda14aad07f9005ac6eeefab4
NguyenVanQuocThanh <pentapperthanh1999@gmail.com>
2020-04-23 21:32:18 +0700

    crud categories

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -4,6 +4,7 @@
 
 @section('body')
 <section id="main-content">
+
     <section class="wrapper">
         <div class="table-agile-info">
             <div class="panel panel-default">
@@ -11,7 +12,18 @@
                     {{ trans('admin/categories/list_cat.title_header') }}
                 </div>
                 <div class="row w3-res-tb">
+                    @if(session('success'))
+                        <div class="alert alert-success">
+                            <p>{{ trans('admin/categories/list_cat.add') }}</p>
+                        </div>
+                    @endif
+                    @if(session('update'))
+                        <div class="alert alert-success">
+                            <p>{{ trans('admin/categories/list_cat.update') }}</p>
+                        </div>
+                    @endif
                     <div class="col-sm-5 m-b-xs">
+                        <button id="" type="button" class="btn btn-default"><a href="{{ route('categories.create') }}">+</a></button>
                         <select class="input-sm form-control w-sm inline v-middle">
                             <option value="0">A-Z</option>
                             <option value="1">Z-A</option>
@@ -19,13 +31,15 @@
                         <button class="btn btn-sm btn-default">{{ trans('admin/categories/list_cat.filter') }}</button>
                     </div>
                     <div class="col-sm-4">
+
                     </div>
                     <div class="col-sm-3">
-                        <div class="input-group">
-                            <input type="text" class="input-sm form-control" placeholder="Search">
+                        <form class="input-group" action="{{ route('categories.index') }}" method="GET">
+                            <input type="hidden" name="action" value="search">
+                            <input name="key" type="text" id="input" class="input-sm form-control" placeholder="Search">
                             <span class="input-group-btn">
-            <button class="btn btn-sm btn-default" type="button">{{ trans('admin/categories/list_cat.search') }}</button>
-          </span>
+                                <button class="btn btn-sm btn-default" type="submit">{{ trans('admin/categories/list_cat.search') }}</button>
+                            </span>
                         </div>
                     </div>
                 </div>
@@ -45,29 +59,32 @@
                                 <th style="width:30px;"></th>
                             </tr>
                         </thead>
+                        @php($i = 0)
+                        @foreach($categories as $category)
                         <tbody>
                             <tr>
                                 <td>
                                     <label class="i-checks m-b-none">
                                         <input type="checkbox" name="post[]"><i></i></label>
                                 </td>
-                                <td>1</td>
-                                <td><span class="text-ellipsis">People</span></td>
-                                <td><span class="text-ellipsis">People Category</span></td>
+                                <td><?php echo ++$i; ?></td>
+                                <td><span class="text-ellipsis">{{$category->title}}</span></td>
+                                <td><span class="text-ellipsis">{{$category->desc}}</span></td>
                                 <td>
-                                    <a href="#" class="active" ui-toggle-class=""><i class="fa fa-pencil-square-o text-info active"></i></a>
-                                    <form action="" method="POST">
+                                    <a href="{{ route('categories.edit', $category->id) }}" class="active" ui-toggle-class=""><i class="fa fa-pencil-square-o text-info active"></i></a>
+                                    <form action="{{ url("categories/$category->id") }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
                                         <button type="submit" title="Delete" onclick="return confirm('Bạn có chắc chắn muốn xóa?');" class="btn-option-user"><i class="fa fa-trash text-danger"></i></button>
                                     </form>
-
                                 </td>
                             </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
                 <div class="pagination-user">
-
-            </div>
+                </div>
             </div>
         </div>
     </section>
